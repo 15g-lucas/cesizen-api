5a408688c27a6bd77185614f3ff1317287a27519
Lucas <gosgnach24lucas@gmail.com>
2025-04-11 12:32:43 +0200

    AuthenticationController

--- routes/api.php
+++ routes/api.php
@@ -1,3 +1,14 @@
 <?php
 
-\Lomkit\Rest\Facades\Rest::resource('users', \App\Rest\Controllers\UsersController::class);
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthenticationController;
+use Lomkit\Rest\Facades\Rest;
+
+Route::post('/register', [AuthenticationController::class, 'register']);
+Route::post('/login', [AuthenticationController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/me', [AuthenticationController::class, 'me']);
+    Route::post('/logout', [AuthenticationController::class, 'logout']);
+    Rest::resource('users', \App\Rest\Controllers\UsersController::class);
+});
